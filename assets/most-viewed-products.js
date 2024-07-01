74bf7ab34d23dc3e9e459c4660951564f7407a0c
Sijan Paudel <sijannpaudel@gmail.com>
2024-07-01 11:50:49 +0545

    updates

--- assets/most-viewed-products.js
+++ assets/most-viewed-products.js
@@ -1,144 +1,144 @@
 class MostViewedProducts extends HTMLElement {
-  constructor() {
-    super();
-    this.storeId = this.dataset.storeid;
-    this.productId = this.dataset.productid;
-    this.id = this.dataset.id;
-    this.getProductsData();
-  }
-
-  async getProductsData() {
-    try {
-      const response = await fetch(
-        `/apps/views-count-dev/mostviewedproducts?store=${this.storeId}`,
-        {
-          method: "GET",
-          headers: {
-            "Content-Type": "application/json",
-            "Access-Control-Allow-Origin": "*",
-          },
-        }
-      );
-
-      if (!response.ok) {
-        throw new Error("Failed to fetch views count");
-      }
-
-      const data = await response.json();
-
-      if (data.products) {
-        this.displayProducts(data.products);
-      }
-    } catch (error) {
-      console.error("Error fetching views count:", error);
+    constructor() {
+        super();
+        this.storeId = this.dataset.storeid;
+        this.productId = this.dataset.productid;
+        this.id = this.dataset.id;
+        this.getProductsData();
     }
-  }
 
-  displayProducts(products) {
-    const productGrid = this.querySelector("#mvpb-product-grid");
-    (async () => {
-      for (const product of products) {
-        if (product.count && product.productHandle) {
-          const handle = product.productHandle;
-          const views = product.count;
+    async getProductsData() {
+        try {
+            const response = await fetch(
+                `/apps/views-count-dev/mostviewedproducts?store=${this.storeId}`,
+                {
+                    method: "GET",
+                    headers: {
+                        "Content-Type": "application/json",
+                        "Access-Control-Allow-Origin": "*",
+                    },
+                }
+            );
 
-          const productUrl = `${window.Shopify.routes.root}products/${handle}.json`;
-
-          try {
-            const response = await fetch(productUrl);
             if (!response.ok) {
-              throw new Error(`Failed to fetch product with handle: ${handle}`);
+                throw new Error("Failed to fetch views count");
             }
 
-            const productData = await response.json();
-            if (productData.product) {
-              const product = productData.product;
-              const { title, image, vendor, url } = product;
+            const data = await response.json();
 
-              // Create the product grid element
-              const productElement = document.createElement("div");
-              productElement.classList.add("mvpb-product-item"); // Add a CSS class for styling
-
-              // Build the product content
-              productElement.innerHTML = `
-                  <a href="/products/${handle}" class="mvpb-product-wrapper" aria-label="View Product">
-                  <div class="mvpb-product-img-wrapper">
-                    <img class="mvpb-product-img" src="${image.src}" alt="${title}">
-                    </div>
-                    <div class="mvpv-product-info">
-                      <h4 class="mvpb-product-title">${title}</h4>
-                      <span class="mvpb-product-brand">Brand: ${vendor}</span>
-                      <span class="mvpb-product-views">Views: ${views}</span>
-                    </div>
-                  </a>
-                `;
-              // Append the product element to the grid
-              productGrid.appendChild(productElement);
-            } else {
-              console.error(`Error fetching product with handle: ${handle}`);
+            if (data.products) {
+                this.displayProducts(data.products);
             }
-          } catch (error) {
-            console.error("Error:", error);
-          }
+        } catch (error) {
+            console.error("Error fetching views count:", error);
         }
-      }
+    }
 
-      if (is_slider) {
-        this.createSlider();
-      }
-    })();
-  }
-  createSlider() {
-    const productGrid = this.querySelector("#mvpb-product-grid");
-    const sliderWrapper = document.createElement("div");
-    sliderWrapper.classList.add("mvpb-slider-wrapper");
+    displayProducts(products) {
+        const productGrid = this.querySelector("#mvpb-product-grid");
+        (async () => {
+            for (const product of products) {
+                if (product.count && product.productHandle) {
+                    const handle = product.productHandle;
+                    const views = product.count;
 
-    const prevButton = document.createElement("button");
-    prevButton.innerText = "Prev";
-    prevButton.classList.add("mvpb-slider-prev");
+                    const productUrl = `${window.Shopify.routes.root}products/${handle}.json`;
 
-    const nextButton = document.createElement("button");
-    nextButton.innerText = "Next";
-    nextButton.classList.add("mvpb-slider-next");
+                    try {
+                        const response = await fetch(productUrl);
+                        if (!response.ok) {
+                            throw new Error(`Failed to fetch product with handle: ${handle}`);
+                        }
 
-    productGrid.parentNode.insertBefore(sliderWrapper, productGrid);
-    sliderWrapper.appendChild(prevButton);
-    sliderWrapper.appendChild(productGrid);
-    sliderWrapper.appendChild(nextButton);
+                        const productData = await response.json();
+                        if (productData.product) {
+                            const product = productData.product;
+                            const { title, image, vendor, url } = product;
 
-    const items = Array.from(productGrid.children);
-    const totalItems = items.length;
-    const slidesPerPage =
-      typeof slides_per_page !== "undefined" ? slides_per_page : 4;
-    let currentSlide = 0;
+                            // Create the product grid element
+                            const productElement = document.createElement("div");
+                            productElement.classList.add("mvpb-product-item"); // Add a CSS class for styling
 
-    // Apply initial styles to the product grid and items
-    productGrid.style.display = "flex";
-    productGrid.style.transition = "transform 0.5s ease";
-    //   productGrid.style.overflow = 'hidden';
+                            // Build the product content
+                            productElement.innerHTML = `
+                    <a href="/products/${handle}" class="mvpb-product-wrapper" aria-label="View Product">
+                    <div class="mvpb-product-img-wrapper">
+                      <img class="mvpb-product-img" src="${image.src}" alt="${title}">
+                      </div>
+                      <div class="mvpv-product-info">
+                        <h4 class="mvpb-product-title">${title}</h4>
+                        <span class="mvpb-product-brand">Brand: ${vendor}</span>
+                        <span class="mvpb-product-views">Views: ${views}</span>
+                      </div>
+                    </a>
+                  `;
+                            // Append the product element to the grid
+                            productGrid.appendChild(productElement);
+                        } else {
+                            console.error(`Error fetching product with handle: ${handle}`);
+                        }
+                    } catch (error) {
+                        console.error("Error:", error);
+                    }
+                }
+            }
 
-    items.forEach((item) => {
-      item.style.flex = `0 0 calc(${100 / slidesPerPage}% - 20px )`;
-      item.style.boxSizing = "border-box";
-    });
+            if (is_slider) {
+                this.createSlider();
+            }
+        })();
+    }
+    createSlider() {
+        const productGrid = this.querySelector("#mvpb-product-grid");
+        const sliderWrapper = document.createElement("div");
+        sliderWrapper.classList.add("mvpb-slider-wrapper");
 
-    const updateSlider = () => {
-      const offset = -currentSlide * (100 / slidesPerPage);
-      productGrid.style.transform = `translateX(${offset}%)`;
-    };
+        const prevButton = document.createElement("button");
+        prevButton.innerText = "Prev";
+        prevButton.classList.add("mvpb-slider-prev");
 
-    prevButton.addEventListener("click", () => {
-      currentSlide = Math.max(currentSlide - 1, 0);
-      updateSlider();
-    });
+        const nextButton = document.createElement("button");
+        nextButton.innerText = "Next";
+        nextButton.classList.add("mvpb-slider-next");
 
-    nextButton.addEventListener("click", () => {
-      currentSlide = Math.min(currentSlide + 1, totalItems - slidesPerPage);
-      updateSlider();
-    });
+        productGrid.parentNode.insertBefore(sliderWrapper, productGrid);
+        sliderWrapper.appendChild(prevButton);
+        sliderWrapper.appendChild(productGrid);
+        sliderWrapper.appendChild(nextButton);
 
-    updateSlider();
-  }
+        const items = Array.from(productGrid.children);
+        const totalItems = items.length;
+        const slidesPerPage =
+            typeof slides_per_page !== "undefined" ? slides_per_page : 4;
+        let currentSlide = 0;
+
+        // Apply initial styles to the product grid and items
+        productGrid.style.display = "flex";
+        productGrid.style.transition = "transform 0.5s ease";
+        //   productGrid.style.overflow = 'hidden';
+
+        items.forEach((item) => {
+            item.style.flex = `0 0 calc(${100 / slidesPerPage}% - 20px )`;
+            item.style.boxSizing = "border-box";
+        });
+
+        const updateSlider = () => {
+            const offset = -currentSlide * (100 / slidesPerPage);
+            productGrid.style.transform = `translateX(${offset}%)`;
+        };
+
+        prevButton.addEventListener("click", () => {
+            currentSlide = Math.max(currentSlide - 1, 0);
+            updateSlider();
+        });
+
+        nextButton.addEventListener("click", () => {
+            currentSlide = Math.min(currentSlide + 1, totalItems - slidesPerPage);
+            updateSlider();
+        });
+
+        updateSlider();
+    }
 }
 
 customElements.define("most-viewed-products", MostViewedProducts);
--- assets/views-count.js
+++ assets/views-count.js
@@ -1,88 +1,88 @@
 if (!customElements.get('views-count-component')) {
-  customElements.define('views-count-component', class CustomViewsCountComponent extends HTMLElement {
-    constructor() {
-      super();
-      this.storeId = this.dataset.storeid;
-      this.productId = this.dataset.productid;
-      this.id = this.dataset.id;
-      this.handle = this.dataset.producthandle;
-      this.isUniqueVisitor = true;
-      if (!CustomViewsCountComponent.requestSent) {
-        CustomViewsCountComponent.requestSent = false;
-        CustomViewsCountComponent.viewsCount = null;
-      }
-      this.requestDebounce = this.debounce(this.sendViewsCountRequest.bind(this), 100);
-    }
-
-    debounce(func, wait) {
-      let timeoutId;
-      return (...args) => {
-        clearTimeout(timeoutId);
-        timeoutId = setTimeout(() => func(...args), wait);
-      };
-    }
-
-    async sendViewsCountRequest() {
-      if (!CustomViewsCountComponent.requestSent) {
-        CustomViewsCountComponent.requestSent = true;
-        try {
-          const response = await fetch(`/apps/views-count-dev/product`, {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json',
-              'Access-Control-Allow-Origin': '*',
-            },
-            body: JSON.stringify({ storeId: this.storeId, productId: this.productId, productHandle: this.handle, vcbToken: this.isUniqueVisitor }),
-          });
-
-          if (!response.ok) {
-            throw new Error('Failed to fetch views count');
-          }
-          const data = await response.json();
-          CustomViewsCountComponent.viewsCount = data.count;
-          this.updateViewsCount();
-        } catch (error) {
-          console.error('Error fetching views count:', error);
-        } finally {
-          CustomViewsCountComponent.requestSent = false;
-        }
-      }
-    }
-
-    connectedCallback() {
-      this.requestDebounce();
-      this.checkLocalStorage();
-    }
-
-    checkLocalStorage() {
-      console.log('checkLocalStorage', localStorage.getItem('vcbItems'));
-      if (localStorage.getItem('vcbItems')) {
-        // Check if the product id is in the local storage array
-        const vcbItems = JSON.parse(localStorage.getItem('vcbItems'));
-        if (vcbItems.includes(this.productId)) {
-          this.isUniqueVisitor = false;
-        } else {
-          vcbItems.push(this.productId);
-          localStorage.setItem('vcbItems', JSON.stringify(vcbItems));
-        }
-      } else {
-        localStorage.setItem('vcbItems', JSON.stringify([this.productId]));
-      }
-    }
-
-    updateViewsCount() {
-      if (CustomViewsCountComponent.viewsCount !== null) {
-        const viewsCountElements = document.querySelectorAll('views-count') || this.querySelector('views-count');
-        if (viewsCountElements && viewsCountElements[0]) {
-          viewsCountElements.forEach((element) => {
-            element.textContent = CustomViewsCountComponent.viewsCount;
-
-          })
+    customElements.define('views-count-component', class CustomViewsCountComponent extends HTMLElement {
+        constructor() {
+            super();
+            this.storeId = this.dataset.storeid;
+            this.productId = this.dataset.productid;
+            this.id = this.dataset.id;
+            this.handle = this.dataset.producthandle;
+            this.isUniqueVisitor = true;
+            if (!CustomViewsCountComponent.requestSent) {
+                CustomViewsCountComponent.requestSent = false;
+                CustomViewsCountComponent.viewsCount = null;
+            }
+            this.requestDebounce = this.debounce(this.sendViewsCountRequest.bind(this), 100);
         }
 
-      } else {
-        console.log('Views count not yet available');
-      }
-    }
-  });
+        debounce(func, wait) {
+            let timeoutId;
+            return (...args) => {
+                clearTimeout(timeoutId);
+                timeoutId = setTimeout(() => func(...args), wait);
+            };
+        }
+
+        async sendViewsCountRequest() {
+            if (!CustomViewsCountComponent.requestSent) {
+                CustomViewsCountComponent.requestSent = true;
+                try {
+                    const response = await fetch(`/apps/views-count-dev/product`, {
+                        method: 'POST',
+                        headers: {
+                            'Content-Type': 'application/json',
+                            'Access-Control-Allow-Origin': '*',
+                        },
+                        body: JSON.stringify({ storeId: this.storeId, productId: this.productId, productHandle: this.handle, vcbToken: this.isUniqueVisitor }),
+                    });
+
+                    if (!response.ok) {
+                        throw new Error('Failed to fetch views count');
+                    }
+                    const data = await response.json();
+                    CustomViewsCountComponent.viewsCount = data.count;
+                    this.updateViewsCount();
+                } catch (error) {
+                    console.error('Error fetching views count:', error);
+                } finally {
+                    CustomViewsCountComponent.requestSent = false;
+                }
+            }
+        }
+
+        connectedCallback() {
+            this.requestDebounce();
+            this.checkLocalStorage();
+        }
+
+        checkLocalStorage() {
+            console.log('checkLocalStorage', localStorage.getItem('vcbItems'));
+            if (localStorage.getItem('vcbItems')) {
+                // Check if the product id is in the local storage array
+                const vcbItems = JSON.parse(localStorage.getItem('vcbItems'));
+                if (vcbItems.includes(this.productId)) {
+                    this.isUniqueVisitor = false;
+                } else {
+                    vcbItems.push(this.productId);
+                    localStorage.setItem('vcbItems', JSON.stringify(vcbItems));
+                }
+            } else {
+                localStorage.setItem('vcbItems', JSON.stringify([this.productId]));
+            }
+        }
+
+        updateViewsCount() {
+            if (CustomViewsCountComponent.viewsCount !== null) {
+                const viewsCountElements = document.querySelectorAll('views-count') || this.querySelector('views-count');
+                if (viewsCountElements && viewsCountElements[0]) {
+                    viewsCountElements.forEach((element) => {
+                        element.textContent = CustomViewsCountComponent.viewsCount;
+
+                    })
+                }
+
+            } else {
+                console.log('Views count not yet available');
+            }
+        }
+    });
 }
